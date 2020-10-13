b1ed7e4bc3f1a106a813fcc1872aaeb82564d6a1
Gibum Kang <Gibum.Kang@creditone.com>
2020-10-13 16:58:34 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Splash from "../components/splash"
+import Header from "../components/header"
+import Footer from "../components/footer"
+import { useStaticQuery, graphql } from "gatsby"
+import "../global/global.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+	const data = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`)
+
+	return (
+		<>
+			<Header siteTitle={data.site.siteMetadata.title || `Title`} />
+			<SEO title="Home" />
+			{/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
+			<Splash />
+			<Footer />
+		</>
+	)
+}
 
 export default IndexPage
